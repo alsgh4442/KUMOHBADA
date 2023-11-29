53428a0ee8cecfafdd89ce017243760fdac3cecc
최연우 <50083524+wafla@users.noreply.github.com>
2023-11-29 17:24:30 +0900

    삭제 기능 추가

--- lib/myitems.dart
+++ lib/myitems.dart
@@ -14,6 +14,12 @@ class _MyItemsTabContentState extends State<MyItemsTabContent> {
     setState(() {});
   }
 
+  void _deleteItem(Item item) {
+    // 아이템 삭제
+    items.remove(item);
+    _refreshState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -55,6 +61,60 @@ class _MyItemsTabContentState extends State<MyItemsTabContent> {
                 Text(user.location),
                 const Spacer(),
                 Text(item.regiTime),
+              ],
+            ),
+            trailing: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                IconButton(
+                  icon: Icon(Icons.edit),
+                  onPressed: () {
+                    // 수정 기능 호출
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => WritingPage(
+                          editItem: item,
+                        ),
+                      ),
+                    ).then((result) {
+                      if (result == true) {
+                        // 화면 refresh
+                        _refreshState();
+                      }
+                    });
+                  },
+                ),
+                IconButton(
+                  icon: Icon(Icons.delete),
+                  onPressed: () {
+                    // 삭제 기능 호출
+                    showDialog(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return AlertDialog(
+                          title: Text('삭제 확인'),
+                          content: Text('이 항목을 삭제하시겠습니까?'),
+                          actions: [
+                            TextButton(
+                              onPressed: () {
+                                _deleteItem(item);
+                                Navigator.of(context).pop();
+                              },
+                              child: Text('삭제'),
+                            ),
+                            TextButton(
+                              onPressed: () {
+                                Navigator.of(context).pop();
+                              },
+                              child: Text('취소'),
+                            ),
+                          ],
+                        );
+                      },
+                    );
+                  },
+                ),
               ],
             ),
           );
